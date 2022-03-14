c386f871828e2606536caa1be597dd1779700de9
首藤　勝史 <katsu19971997@gmail.com>
2022-03-14 18:14:57 +0900

    投稿機能の追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,5 +4,20 @@ class BoardsController < ApplicationController
     end
 
     def new
+      @board = Board.new
+    end
+
+    def create
+      @board = Board.new(board.params)
+      if @board.save
+        redirect_to board_path(@board)
+      else
+        render :new, notice:
+      end
+    end
+
+    private
+    def board_params
+      params.require(:board).permit(:titlename, :content)
     end
 end
