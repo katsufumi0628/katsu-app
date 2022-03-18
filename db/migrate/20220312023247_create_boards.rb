3a6d21604bccd53bbcb673e334d7a8b1f0f87d8f
首藤　勝史 <katsu19971997@gmail.com>
2022-03-18 17:51:35 +0900

    hasmany + showアクションの修正

--- db/migrate/20220312023247_create_boards.rb
+++ db/migrate/20220312023247_create_boards.rb
@@ -1,7 +1,6 @@
 class CreateBoards < ActiveRecord::Migration[6.0]
   def change
     create_table :boards do |t|
-      t.references :user, null: false
       t.string :titlename
       t.text :content
       t.timestamps
